4ea4496780f93bcf32ff180b554c24257c77c237
Dima Savkov <dimsaw85@gmail.com>
2021-11-13 16:47:01 +0200

    finish1

--- src 2/js/01-gallery.js
+++ src 2/js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const allPhotos = document.querySelector(".gallery");
+const galleryPhotos = createPhotosGalerry(galleryItems);
+
+allPhotos.insertAdjacentHTML("afterbegin", galleryPhotos);
+allPhotos.addEventListener("click", clickOnPhoto);
+
+function createPhotosGalerry(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
+
+function clickOnPhoto(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return console.log("это не фото");
+  }
+
+  const url = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${url}" width="800" height="600">
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscPress);
+        function onEscPress(event) {
+          const ESC_KEY_CODE = "Escape";
+          if (event.code === ESC_KEY_CODE) {
+            instance.close();
+          }
+        }
+      },
+    }
+  );
+
+  console.log("клик");
+  instance.show();
+}
--- src 2/js/02-lightbox.js
+++ src 2/js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const allPhotos = document.querySelector(".gallery");
+const galleryPhotos = createPhotosGalerry(galleryItems);
+
+allPhotos.insertAdjacentHTML("afterbegin", galleryPhotos);
+
+function createPhotosGalerry(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>
+        `;
+    })
+    .join("");
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
